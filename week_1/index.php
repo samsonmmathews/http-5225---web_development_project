6c7fe6c40d258c6fdd2cc90761fde7438f60e701
Samson Mathews <samsonmmathews@gmail.com>
2025-09-10 16:54:15 -0400

    Week 1

--- week_1/index.php
+++ week_1/index.php
@@ -6,6 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <script>
+        let fname = "SAMSON";
+        console.log(fname);
+        document.write("Welcome back, " + fname + "<br>");
+    </script>
+
+    <?php
+        $fname = "Samson";
+        echo "Welcome back, " . $fname;
+        echo "<h1 style=''>" .$fname . "</h1>"
+    ?>
 </body>
 </html>
