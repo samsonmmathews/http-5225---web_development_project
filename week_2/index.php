d8ba69683bb0d0b6276f29392ccac988606e9f83
Samson Mathews <samsonmmathews@gmail.com>
2025-09-18 14:32:48 -0400

    week 2

---
+++ week_2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 3</title>
+    <style>
+        .box {
+            border: 1px solid orange;
+            box-sizing:
+        }
+</head>
+<body>
+    <?php
+        $hour = date('g');
+        echo $hour;
+    ?>
+    <script>
+        const date = new Date();
+
+</body>
+</html>
