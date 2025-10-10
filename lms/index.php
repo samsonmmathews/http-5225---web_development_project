41929da23a4bcd628f557b8281325cb87323c7a6
Samson Mathews <samsonmmathews@gmail.com>
2025-10-10 10:47:53 -0400

    Displayed all columns and added update, delete buttons

--- lms/index.php
+++ lms/index.php
@@ -20,12 +20,40 @@
             // echo '<pre>' . print_r($schools) . '</pre>';
 
             foreach ($schools as $school) {
-                echo $school['School Name'] . 
-                '<form>
-                <input type="hidden" name="id" value="123">
-                <input type="submit" value="Edit">
-                </form>' . 
-                '<br>';
+                echo '<div class="col-md-4 mt-2 mb-2">
+                    <div class="card">
+                      <div class="card-body">
+                        <h3 class="card-title">School Name: ' . $school['School Name'] . '</h3>
+                        <h4 class="card-title">Board Name: ' . $school['Board Name'] . '</h4>
+                        <p class="card-text">School Number: ' . $school['School Number'] . '</p>
+                        <p class="card-text">School Level: ' . $school['School Level'] . '</p>
+                        <span class="badge bg-secondary">School Language: ' . $school['School Language'] . '</span>
+                        <span class="badge bg-info">, School Type: ' . $school['School Type'] . '</span>
+                        <p class="card-text">School Special Conditions: ' . $school['School Special Conditions'] . '</p>
+                        <span class="badge bg-secondary">Phone: ' . $school['Phone'] . '</span>
+                        <span class="badge bg-info">, Email: ' . $school['Email'] . '</span>
+                        <p class="card-text">Fax: ' . $school['Fax'] . '</p>
+                        <p class="card-text">Address: ' . $school['Street'] . ', ' . $school['City'] . ', ' . $school['Province'] . ', ' . $school['Postal Code'] .'</p>
+                        <p class="card-text">Grade Range: ' . $school['Grade Range'] . '</p>
+                      </div>
+                      <div class="card-footer">
+                        <div class="row">
+                          <div class="col">
+                            <form action="updateschool.php">
+                              <input type="hidden" name="id" value="' . $school['id'] . '">
+                              <button type="submit" name="updateSchool" class="btn btn-sm btn-primary">Update</button>
+                            </form>
+                          </div>
+                          <div class="col text-end">
+                            <form action="deleteschool.php" method="GET">
+                                <input type="hidden" name="id" value="' . $school['id'] . '">
+                                <button type="submit" name="deleteSchool" class="btn btn-sm btn-danger">Delete</button>
+                              </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>'; 
             };
         ?>
         <style>
