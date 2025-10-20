e96ec41ee1a5c5c48c80a8bddc5e698f53f89f06
Samson Mathews <samsonmmathews@gmail.com>
2025-10-20 16:05:19 -0400

    Added login, logout, register pages

--- lms/nav.php
+++ lms/nav.php
@@ -1,6 +1,12 @@
 <nav>
     <div>
         <a href="index.php" class="btn btn-primary btn-sm">Home</a>
-        <a href="addschool.php" class="btn btn-primary btn-sm">Add School</a>
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="addschool.php" class="btn btn-primary btn-sm">Add School</a>
+            <a href="logout.php" class="btn btn-danger btn-sm">Logout (<?php echo $_SESSION['user_name']; ?>)</a>
+        <?php else: ?>
+            <a href="login.php" class="btn btn-success btn-sm">Login</a>
+            <a href="register.php" class="btn btn-secondary btn-sm">Register</a>
+        <?php endif; ?>
     </div>
 </nav>
