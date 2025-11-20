b639ea02125894904f80ed49dc727664806246d9
Samson Mathews <samsonmmathews@gmail.com>
2025-11-20 12:07:37 -0500

    Laravel practice

--- laralms/database/migrations/2025_11_19_204646_create_students_table.php
+++ laralms/database/migrations/2025_11_19_204646_create_students_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('email');
+            // $table->softDeletes();
             $table->timestamps();
         });
     }
