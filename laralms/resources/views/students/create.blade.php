223c5371a1fdf7a6a6c57f4ff650069ba57bd391
Samson Mathews <samsonmmathews@gmail.com>
2025-12-07 15:31:40 -0500

    Update template and bootstrap code

--- laralms/resources/views/students/create.blade.php
+++ laralms/resources/views/students/create.blade.php
@@ -1,14 +1,45 @@
 @extends('template')
 @section('content')
 <h3>Add a Student</h3>
-<form action="{{ route('students.store') }}"
-method="POST">
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            @if($errors -> any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors -> all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+
+<form action="{{ route('students.store') }}" method="POST">
     {{ csrf_field() }}
     <input type="text" name="fname" placeholder="First Name">
+    @error('fname')
+        <span class="text-danger">
+            {{ $message }}
+        </span>
+    @enderror
     <br><br>
     <input type="text" name="lname" placeholder="Last Name">
+    @error('lname')
+        <span class="text-danger">
+            {{ $message }}
+        </span>
+    @enderror
     <br><br>
     <input type="email" name="email" placeholder="Email">
+    @error('email')
+        <span class="text-danger">
+            {{ $message }}
+        </span>
+    @enderror
     <br><br>
     <input type="submit" value="Add Student">
 </form>
--- laralms/resources/views/students/index.blade.php
+++ laralms/resources/views/students/index.blade.php
@@ -1,16 +1,26 @@
 @extends('template')
 @section('content')
     <h1>All Students</h1>
-
-    @foreach ($students as $student)
-        {{ $student -> fname }} {{ $student -> lname }} 
-        <a href="{{ route('students.edit', $student -> id ) }}" class="card-link">Edit</a>
-        {{-- <a href="{{ route('students.trash', $student -> id )}}" class="card-link">Delete</a> --}}
-        <form action="{{ route('students.destroy', $student -> id ) }}" method="post">
-            <input type="submit" value="Delete" />
-            @method('delete')
-            @csrf
-        </form>
-        <br>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            @foreach ($students as $student)
+                <div class="col-sm-3">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $student -> fname }} {{ $student -> lname }} </h5>
+                            <a href="mailto:{{ $student -> email }}" class="btn btn-primary">{{ $student -> email }}</a>
+                        </div>
+                        <div class="card-footer">
+                            <a href="{{ route('students.edit', $student -> id ) }}" class="btn btn-sm btn-success">Edit</a>
+                            {{-- <form action="{{ route('students.destroy', $student -> id ) }}" method="post">
+                                <input type="submit" value="Delete" />
+                                @method('delete')
+                                @csrf
+                            </form> --}}
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
